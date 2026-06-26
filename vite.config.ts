2d5e8d2625ba565bedd97e84b97dfbace954a6e2
joaq73-lang <joaquinpalominocesaremmerson@gmail.com>
2026-06-25 23:51:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/buenaventura-help-desk/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
